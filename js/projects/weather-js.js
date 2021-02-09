4981d37bd27c6167b61fef2b21891b4eaf3817b2
geekymayur <rajmayur219@gmail.com>
2021-02-09 21:02:47 +0530

    weather app working and some background image added

--- js/projects/weather-js.js
+++ js/projects/weather-js.js
@@ -1,21 +1,35 @@
 function profile() {
-    let greet = document.getElementById("greet");
-    let name = prompt("Hey, I am Mayur and You ?");
-    if (name == "" || name == null) {
-        greet.innerHTML = `Welcome User`;
-    } else if (name == "anshu" || name == "Anshu" || name == "ANSHU") {
-        alert("I Love You ")
-    }
-    else {
-        greet.innerHTML = `Welcome ${name}`;
-    }
-
+    // let greet = document.getElementById("greet");
+    // let name = prompt("Hey, I am Mayur and You ?");
+    // if (name == "" || name == null) {
+    //     greet.innerHTML = `Welcome User`;
+    // } else {
+    //     greet.innerHTML = `Welcome ${name}`;
+    // }
+    $("#display-box-1").hide()
 }
+
+
+
+
+// Calling profile function for greeting user
+
 profile();
+
 
 let checkbutton = document.getElementById("check-button");
 let getcityname = document.getElementById("cityname");
+let weatherImage = document.getElementById("weatherImage");
+let showTemp = document.getElementById("showTemp");
+let showCityName = document.getElementById("cityName");
+let showDiscription = document.getElementById("showDiscription");
 
+// get geolocation weather
+
+
+
+
+// this function will run after clicking on check button 
 let getWeather = async (event) => {
     event.preventDefault();
     let cityname = getcityname.value;
@@ -23,17 +37,51 @@ let getWeather = async (event) => {
         alert("Enter City name");
     }
     else {
+
         try {
-            let url = `api.openweathermap.org/data/2.5/weather?q=${cityname}&units=imperial&appid=a02caaedb7f5180cd72e9ece23c352fa`;
+            let url = `http://api.openweathermap.org/data/2.5/weather?q=${cityname}&units=metric&appid=a02caaedb7f5180cd72e9ece23c352fa`;
             let response = await fetch(url);
             let data = await response.json();
+            console.log(url);
             console.log(data);
+            $("#display-box-1").show().slideDown();
+
+            let celcius = data.main.temp;
+            console.log(typeof celcius)
+            if (celcius[1] == 0) {
+                showTemp.innerHTML = `0°C`;
+            } else {
+                showTemp.innerHTML = `${celcius.toFixed(0)}°C`;
+
+            }
+            if (data.sys.country == undefined) {
+                showCityName.innerHTML = `${data.name}`;
+
+            } else {
+                showCityName.innerHTML = `${data.name}, ${data.sys.country}`;
+            }
+            weatherImage.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`;
+            showDiscription.innerHTML = `${data.weather[0].description}`;
+            $("#sky").text(data.weather[0].main);
+            let feelslike = data.main.feels_like;
+            let mintemp = data.main.temp_min;
+            let maxtemp = data.main.temp_max;
+
+            $("#feelsLike").text(feelslike.toFixed(0) + "°C");
+            $("#minTemp").text(mintemp.toFixed(0) + "°C");
+            $("#maxTemp").text(maxtemp.toFixed(0) + "°C");
+            $("#humidity").text(data.main.humidity + " %");
+            $("#windspeed").text(data.wind.speed + " meter/sec");
         } catch (error) {
-            alert("Please Check Your City Name and Check Again.");
+            $("#display-box-1").hide();
+
+            alert("Please Check Your City Name and Try Again or check your Internet Connection.");
             console.log(error);
         }
     }
 };
+
+
 checkbutton.addEventListener("click", getWeather);
 
 
